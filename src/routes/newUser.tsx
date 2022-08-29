6dbc2386f748104aa000e66080bdc56459b8f261
tharunoptimus <tharunoptimus@outlook.com>
2022-08-29 21:20:58 +0530

    added stuff to newUser and its module css rn gets fingerprint and stores in idb need to work on the server part

--- src/routes/newUser.tsx
+++ src/routes/newUser.tsx
@@ -1,21 +1,39 @@
 import { useEffect } from "react"
 import { useNavigate } from "react-router-dom"
+import styles from "./styles/newUser.module.css"
+import "./styles/newUser.module.css"
+import cat from "../assets/gifs/cat.gif"
+import { setStorage } from "../utilities/indexedDb"
+
+import { ClientJS } from 'clientjs';
 
 function NewUser() {
 
   let navigate = useNavigate()
 
   useEffect(() => {
+    
+    let client = new ClientJS();
+    let ua = client.getUserAgent()
+		let canvasPrint = client.getCanvasPrint();
+
+		let fingerprint = client.getCustomFingerprint(ua, canvasPrint);
+
+		console.log(fingerprint);
     console.log("Registering new user")
 
-    setTimeout(() => {
-      navigate("/")
-    }, 3000)
+    setStorage("fingerprint", fingerprint)
+   
   })
 
   return (
-    <div className="loading">
-      Getting things ready for the new pope.
+    <div className={styles.wrapper}>
+      <img className={styles.catGif} src={cat} alt="Loading Content" height={150} width={225} />
+
+      <span className={styles.loadingText}>Loading...</span>
+
+      <span className={styles.infoSpan}>Setting things up for the new Siker ⚡</span>
+
     </div>
   )
 }
