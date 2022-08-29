3c6cf1839dd03ec5e003741202a7c2340df9f74d
tharunoptimus <tharunoptimus@outlook.com>
2022-08-29 20:23:24 +0530

    Added route logic to show homepage if no user found in IDB

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,35 @@
 import AppComponent from "./assets/components/AppComponent";
+import LoadingPage from "./assets/components/loadingPage";
+import { getStorage, setStorage } from "./utilities/indexedDb";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function App() {
+
+  let navigate = useNavigate()
+  let [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(async () => {
+      
+      let user = await getStorage("user")
+      if(!user) {
+        navigate("/home")
+      }
+      setIsLoading(false)
+
+    }, 2000)
+  })
+
+  if(isLoading) {
+    return (<LoadingPage />)
+  } else {
+    return (<AppComponent />)
+  }
 
   // if user is authorized 
     // if location is enabled
       // render appComponent
     // render noLocation
   // render newUser
-
-  return (
-    <AppComponent />
-  );
 }
