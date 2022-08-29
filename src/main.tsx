b79bb383ebbc26fcfb4fd01be5cb2b4cde534d07
tharunoptimus <tharunoptimus@outlook.com>
2022-08-29 20:23:07 +0530

    Added route for homepage

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import ReactDOM from "react-dom/client"
+import "./index.css"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import App from "./App"
 import AboutSike from "./routes/aboutSike"
@@ -6,6 +7,7 @@ import NewSike from "./routes/newSike"
 import NewUser from "./routes/newUser"
 import NoLocation from "./routes/noLocation"
 import Sike from "./routes/sike"
+import HomePage from "./assets/components/homePage"
 
 import NotFoundError from "./routes/notFoundError"
 
@@ -17,6 +19,7 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 			<Route path="newSike" element={<NewSike />} />
 			<Route path="newUser" element={<NewUser />} />
 			<Route path="noLocation" element={<NoLocation />} />
+			<Route path="home" element={<HomePage />} />
 			<Route path="sikes/:sikeId" element={<Sike />} />
 			
       <Route path="*" element={<NotFoundError />} />
