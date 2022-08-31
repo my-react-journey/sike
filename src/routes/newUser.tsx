049aa5adc580600edae03f608b5059d934ce1e68
tharunoptimus <tharunoptimus@outlook.com>
2022-08-31 13:37:32 +0530

    newUser works fine

--- src/routes/newUser.tsx
+++ src/routes/newUser.tsx
@@ -1,41 +1,65 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
 import styles from "./styles/newUser.module.css"
 import "./styles/newUser.module.css"
 import cat from "../assets/gifs/cat.gif"
 import { setStorage } from "../utilities/indexedDb"
+import endpoint from "../APIEndpoint"
 
-import { ClientJS } from 'clientjs';
+import { ClientJS } from "clientjs"
 
 function NewUser() {
+	let navigate = useNavigate()
 
-  let navigate = useNavigate()
+	let [status, setStatus] = useState("Setting things up for the new Siker ⚡")
 
-  useEffect(() => {
-    
-    let client = new ClientJS();
-    let ua = client.getUserAgent()
-		let canvasPrint = client.getCanvasPrint();
+	useEffect(() => {
+		setTimeout(async () => {
+			let client = new ClientJS()
+			let ua = client.getUserAgent()
+			let canvasPrint = client.getCanvasPrint()
 
-		let fingerprint = client.getCustomFingerprint(ua, canvasPrint);
+			let fingerprint = client.getCustomFingerprint(ua, canvasPrint)
+			setStorage("fingerprint", fingerprint)
 
-		console.log(fingerprint);
-    console.log("Registering new user")
+			console.log(`Registering user: ${fingerprint}`)
+			let data
+			try {
+				let response = await fetch(`${endpoint}/user`, {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify({
+						fingerprint
+					})
+				})
+				data = await response.json()
+			} catch (error) {
+				return setStatus("Error setting up, please try again")
+			}
+			
+			await setStorage("user", data)
+			navigate("/")
 
-    setStorage("fingerprint", fingerprint)
-   
-  })
+		}, 0)
+	})
 
-  return (
-    <div className={styles.wrapper}>
-      <img className={styles.catGif} src={cat} alt="Loading Content" height={150} width={225} />
+	return (
+		<div className={styles.wrapper}>
+			<img
+				className={styles.catGif}
+				src={cat}
+				alt="Loading Content"
+				height={150}
+				width={225}
+			/>
 
-      <span className={styles.loadingText}>Loading...</span>
+			<span className={styles.loadingText}>Loading...</span>
 
-      <span className={styles.infoSpan}>Setting things up for the new Siker ⚡</span>
-
-    </div>
-  )
+			<span className={styles.infoSpan}>{status}</span>
+		</div>
+	)
 }
 
 export default NewUser
