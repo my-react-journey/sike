457cbd8c095e41ffeb8bb8c81f349470f002c1a0
tharunoptimus <tharunoptimus@outlook.com>
2022-08-31 16:04:21 +0530

    Added location logic to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,42 @@
-import AppComponent from "./assets/components/AppComponent";
-import LoadingPage from "./assets/components/loadingPage";
-import { getStorage, setStorage } from "./utilities/indexedDb";
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import AppComponent from "./assets/components/AppComponent"
+import LoadingPage from "./assets/components/loadingPage"
+import { getStorage, setStorage } from "./utilities/indexedDb"
+import { getCurrentPosition } from "./utilities/geolocation"
+import { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
 
 export default function App() {
+	let navigate = useNavigate()
+	let [isLoading, setIsLoading] = useState(true)
 
-  let navigate = useNavigate()
-  let [isLoading, setIsLoading] = useState(true)
+	useEffect(() => {
+		setTimeout(async () => {
+			let user = await getStorage("user")
+			if (!user) {
+				return navigate("/home")
+			}
 
-  useEffect(() => {
-    setTimeout(async () => {
-      
-      let user = await getStorage("user")
-      if(!user) {
-        navigate("/home")
-      }
-      setIsLoading(false)
+			let locationData : any
+			try {
+				locationData = await getCurrentPosition()
+				setIsLoading(false)
+				return true
+			} catch (error) {
+				return navigate("/noLocation")
+			}
 
-    }, 2000)
-  })
+		}, 2000)
+	})
 
-  if(isLoading) {
-    return (<LoadingPage />)
-  } else {
-    return (<AppComponent />)
-  }
+	if (isLoading) {
+		return <LoadingPage />
+	} else {
+		return <AppComponent />
+	}
 
-  // if user is authorized 
-    // if location is enabled
-      // render appComponent
-    // render noLocation
-  // render newUser
+	// if user is authorized
+	// if location is enabled
+	// render appComponent
+	// render noLocation
+	// render newUser
 }
