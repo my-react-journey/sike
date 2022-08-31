a128c5f6275531fa1d1edb946d18606e7852929a
tharunoptimus <tharunoptimus@outlook.com>
2022-08-31 21:13:13 +0530

    Changed pencil from emoji to webp removed the plus icon

--- src/assets/components/AppComponent.tsx
+++ src/assets/components/AppComponent.tsx
@@ -1,45 +1,54 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom"
 import styles from "./componentStyles/appcomponent.module.css"
 import sikeLogo from "../../assets/components/componentImages/sikeLogo.webp"
+import pencil from "../../assets/components/componentImages/pencil.webp"
 import settingsSVG from "../../assets/components/svgs/settings.svg"
 
 export default function AppComponent() {
+	let navigate = useNavigate()
 
-  let navigate = useNavigate()
+	let createNew = () => {
+		navigate("/newSike")
+	}
 
-  let createNew = () => {
-    navigate("/newSike")
-  }
+	return (
+		<>
+			<div className={styles.wrapper}>
+				<div className={styles.header}>
+					<img
+						title="Tap to see your profile details"
+						aria-label="Tap to see your profile details"
+						src={sikeLogo}
+						alt="Sike Logo"
+						height={40}
+						width={40}
+					/>
 
-    return (
-      <>
+					<span className={styles.appText}>Sike</span>
 
-      <div className={styles.wrapper}>
+					<div className={styles.centerButtons}></div>
 
-        <div className={styles.header}>
-          <img title="Tap to see your profile details" aria-label="Tap to see your profile details" src={sikeLogo} alt="Sike Logo" height={40} width={40}/>
+					<img
+						title="Tap to modify app settings"
+						aria-label="Tap to modify app settings"
+						src={settingsSVG}
+						alt="Settings"
+						height={30}
+						width={30}
+					/>
+				</div>
 
-          <span className={styles.appText}>Sike</span>
+				<div className={styles.sikesContainer}></div>
 
-          <div className={styles.centerButtons}>
-            
-          </div>
-
-          <img title="Tap to modify app settings" aria-label="Tap to modify app settings" src={settingsSVG} alt="Settings" height={30} width={30}/>
-        </div>
-
-        <div className={styles.sikesContainer}>
-
-
-        </div>
-
-        <div title="Tap to create a new sike!" aria-label="Tap to create a new sike!" onClick={createNew} className={styles.createButton}>
-          <span className={styles.pencilEmoji}>✏️</span>
-          <span className={styles.plusIcon}>+</span>
-        </div>
-        
-      </div>
-
-      </>
-    );
-  }
+				<div
+					title="Tap to create a new sike!"
+					aria-label="Tap to create a new sike!"
+					onClick={createNew}
+					className={styles.createButton}
+				>
+					<img src={pencil} alt="Pencil - Create new sike" height={30} width={30} />
+				</div>
+			</div>
+		</>
+	)
+}
