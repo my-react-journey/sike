3fcdadbead7cb33e70ad8c2f169ea18a0fb2361f
tharunoptimus <tharunoptimus@outlook.com>
2022-09-01 17:47:09 +0530

    Added UI to `newSike`

--- src/routes/newSike.tsx
+++ src/routes/newSike.tsx
@@ -1,8 +1,16 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom"
+import styles from "./styles/newSike.module.css"
+import campaign from "../assets/components/svgs/campaign.svg"
+import back from "../assets/components/svgs/back.svg"
+import { useEffect, useRef } from "react"
 
 export default function NewSike() {
+	let inputLimit: number = 280
 
-  let navigate = useNavigate()
+	let sikeInputTextarea = useRef<HTMLTextAreaElement>(null)
+	let inputRemaining = useRef<HTMLSpanElement>(null)
+
+	let navigate = useNavigate()
   let postSike = () => {
 
     // get post content, check for abnomalities,
@@ -11,13 +19,46 @@ export default function NewSike() {
 
     navigate(-1)
   }
-  
-  return (
-    <>
-      <input type="text" placeholder="Type something" />    
 
-      <button type="button" onClick={postSike}>Say Sike</button>
-    
-    </>
-  );
+	return (
+		<>
+			<div className={styles.wrapper}>
+				<div className={styles.header}>
+					<img
+						src={back}
+						alt="back"
+						height={30}
+						width={30}
+						onClick={() => navigate(-1)}
+					/>
+					<span className={styles.appText}>Post new Sike</span>
+					<img
+						src={campaign}
+						alt="Post new Sike"
+						height={30}
+						width={30}
+					/>
+				</div>
+
+				<div className={styles.sikeInputContainer}>
+					<textarea
+						ref={sikeInputTextarea}
+						onInput={handleInput}
+						className={styles.sikeInput}
+						placeholder="What's on your mind?"
+						title="Type what's on your mind."
+						aria-label="Type what's on your mind."
+					></textarea>
+
+					<span className={styles.inputCharsRemaining}>
+						<span ref={inputRemaining} className={styles.inputRemaining}>280</span>
+						<span className={styles.inputLimit}>
+							{" "}
+							/ {inputLimit}
+						</span>
+					</span>
+				</div>
+			</div>
+		</>
+	)
 }
