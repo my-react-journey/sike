519e2ae6fb0a2d75554a636b47f6aa404772e880
tharunoptimus <tharunoptimus@outlook.com>
2022-09-01 21:11:04 +0530

    Added utility types

---
+++ src/utilities/types.ts
@@ -0,0 +1,21 @@
+export type SikeProps = {
+	comments: string[],
+	createdAt: string,
+	downvotedBy: string[],
+	location: {
+		type: string, 
+		coordinates: number[]
+	},
+	madeBy: string,
+	name: string,
+	profilePic: string,
+	text: string,
+	totalUpvotes: number,
+	upvotedBy: string[],
+	__v: number,
+	_id: string
+}
+
+export type SikeCardProps = {
+	sike: SikeProps
+}
