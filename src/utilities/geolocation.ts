79e6b0f5da5c5e021c901de5eaf6bc52731fcff3
tharunoptimus <tharunoptimus@outlook.com>
2022-09-01 18:33:16 +0530

    Added type to `getCurrentPosition`

--- src/utilities/geolocation.ts
+++ src/utilities/geolocation.ts
@@ -1,4 +1,4 @@
-export function getCurrentPosition() {
+export function getCurrentPosition() : Promise<GeolocationPosition>{
     return new Promise((resolve, reject) => {
         navigator.geolocation.getCurrentPosition(
             (position) => resolve(position),
